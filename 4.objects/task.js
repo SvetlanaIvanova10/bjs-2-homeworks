95449efc3a0b7246a0606eadcc5157f747a35b90
Svetlana Ivanova <fascinatedsveta@gmail.com>
2022-08-21 20:45:27 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((acc, item, index, arr) =>{
+    acc+=item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    } else {
+      return acc;
+    } 
+  }, 0);
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
